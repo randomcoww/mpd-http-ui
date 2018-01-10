27e4c5062f93242924beb844a28d2768d21e960f
randomcoww <etherblimp@gmail.com>
2018-01-09 22:22:56 -0800

    update

--- watch.go
+++ watch.go
@@ -9,58 +9,79 @@ import (
   "strings"
 )
 
-var (
-  logFile = "log"
-  updated chan string
+
+type Watcher struct {
+  added   chan string
   deleted chan string
+}
+
+var (
+  addedString = "update: added "
+  deletedString = "update: removing "
 )
 
-func main() {
+
+func NewWatcher(logFile string) (*Watcher, error) {
   syscall.Mkfifo(logFile, 0600)
 
   f, err := os.OpenFile(logFile, os.O_CREATE, os.ModeNamedPipe)
   if err != nil {
-    panic(err)
+    return nil, err
   }
-
-  updated = make(chan string)
-  deleted = make(chan string)
 
   reader := bufio.NewReader(f)
 
-  go readChannel()
-  readLog(reader)
+  w := &Watcher{
+    added: make(chan string),
+    deleted: make(chan string),
+  }
+
+  go w.readLog(reader)
+
+  return w, nil
 }
 
 
-func readLog(reader *bufio.Reader) {
+func (w *Watcher) readLog(reader *bufio.Reader) {
   for {
     line, err := reader.ReadString('\n')
+
     if err != nil {
-      time.Sleep(50 * time.Millisecond)
+      time.Sleep(100 * time.Millisecond)
       continue
     }
 
-    if strings.HasPrefix(line, "updated: ") {
-      updated <- strings.TrimPrefix(line, "updated: ")
+    if strings.HasPrefix(line, addedString) {
+      w.added <- strings.TrimPrefix(line, addedString)
 
-    } else if strings.HasPrefix(line, "deleted: ") {
-      deleted <- strings.TrimPrefix(line, "deleted: ")
+    } else if strings.HasPrefix(line, deletedString) {
+      w.deleted <- strings.TrimPrefix(line, deletedString)
     }
   }
 }
 
 
-func readChannel() {
+func (w *Watcher) readChannel() {
   for {
     select {
-    case c := <- updated:
-      fmt.Println("update ", c)
+    case c := <- w.added:
+      fmt.Println("add", c)
 
-    case c := <- deleted:
-      fmt.Println("delete ", c)
+    case c := <- w.deleted:
+      fmt.Println("delete", c)
 
-    case <-time.After(1 * time.Second):
+    case <-time.After(100 * time.Millisecond):
     }
   }
+}
+
+
+func main() {
+  w, err := NewWatcher("log")
+
+  if err != nil {
+    panic(err)
+  }
+
+  w.readChannel()
 }
