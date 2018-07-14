9de5129842458e2c806c9c41b10b02a69c6b78c8
randomcoww <etherblimp@gmail.com>
2018-07-13 19:02:36 -0700

    Basic UI layout

--- mpd-ui/src/app/app-routing.module.ts
+++ mpd-ui/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PlaylistComponent } from './playlist/playlist.component';
 import { DatabaseSearchComponent } from './database-search/database-search.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/playlist', pathMatch: 'full' },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'playlist', component: PlaylistComponent },
-  { path: 'database-search', component: DatabaseSearchComponent }
+  { path: 'database-search', component: DatabaseSearchComponent },
+  { path: 'main', component: MainComponent }
 ];
 
 @NgModule({
