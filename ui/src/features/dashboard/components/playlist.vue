7fec0296bb05b0436dd5d1bc6ab0e313a66dea9b
randomcoww <etherblimp@gmail.com>
2018-07-16 02:05:41 -0700

    migrate to vuejs - adapt sample project https://github.com/prograhammer/vue-pizza

---
+++ ui/src/features/dashboard/components/playlist.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+v-card-text
+  v-layout(row wrap style="align-items: center;")
+    template(v-for="playlistitem in playlistitems")
+      v-flex(d-flex xs12 sm12 md4)
+        | {{playlistitem.Artist}}
+      v-flex(d-flex xs12 sm12 md8)
+        v-layout(style="align-items: center;")
+          v-flex.text-xs-left(md10) {{playlistitem.Title}}
+          v-flex.text-xs-right(md2)
+            v-btn(flat icon color="primary")
+              v-icon delete
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      playlistitems: [],
+      errored: false
+    }
+  },
+
+  mounted () {
+    axios.get('http://localhost:3000/playlist/items', {
+      params: {
+        start: -1,
+        end: -1
+      }
+    }).then(response => {
+      this.playlistitems = response.data
+    }).catch(error => {
+      console.log(error)
+      this.errored = true
+    })
+  }
+}
+</script>
