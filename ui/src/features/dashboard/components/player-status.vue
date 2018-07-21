8bf75c629d8f87a98f47fd113780f17977d24160
randomcoww <etherblimp@gmail.com>
2018-07-21 02:03:49 -0700

    Make seek slider control work

--- ui/src/features/dashboard/components/player-status.vue
+++ ui/src/features/dashboard/components/player-status.vue
@@ -1,14 +1,45 @@
 <template lang="pug">
 v-card-text
-  v-slider(:value="seek")
+  v-slider(
+    :max="seek_duration"
+    :value="seek_elaspsed"
+    v-on:mousedown="onmousedown"
+    v-on:change="onchange")
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      dragStartValue: null
+    }
+  },
+
   computed: {
-    seek () {
-      return this.$store.state.websocket.socket.seek
+    seek_elaspsed () {
+      if (this.dragStartValue != null) {
+        return this.dragStartValue
+      } else {
+        return this.$store.state.websocket.socket.elapsed
+      }
+    },
+    seek_duration () {
+      return this.$store.state.websocket.socket.duration
+    }
+  },
+
+  methods: {
+    onchange (value) {
+      console.info('slider_changed', value)
+
+      this.dragStartValue = null
+      this.$socket.sendObj({ mutation: 'seek', value: value })
+      this.$store.commit('elapsed', { value: value })
+    },
+    onmousedown () {
+      console.info('dragging')
+      this.dragStartValue = this.$store.state.websocket.socket.elapsed
     }
   }
 }
