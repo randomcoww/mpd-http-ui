899b68b34d6cb0ad34bce7b5c51a064159594250
randomcoww <etherblimp@gmail.com>
2018-07-22 03:17:51 -0700

    Tweak playlist infinite scroll

--- ui/src/features/dashboard/components/playlist.vue
+++ ui/src/features/dashboard/components/playlist.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-v-card
+v-card.playlist
   v-card-title
     .title Playlist
   v-card-text
-    virtual-list(:size="64" :remain="this.buffer" :onscroll="onscroll" :tobottom="tobottom" :totop="totop")
+    virtual-list(:size="64" :remain="this.buffer" :bench="this.bench" :onscroll="onscroll" :tobottom="tobottom" :totop="totop")
       ul(v-for="playlistitem in playlistitems")
         v-flex(d-flex)
           v-layout(align-center justify-center row fill-height)
@@ -27,6 +27,7 @@ export default {
 
   data () {
     return {
+      bench: 100,
       start: 0,
       end: 40,
       buffer: 12,
@@ -42,22 +43,24 @@ export default {
   },
 
   created () {
-    let end = this.end + this.buffer
+    let end = this.end + this.bench
     this.$socket.sendObj({ mutation: 'playlist', value: [this.start, end] })
   },
 
   methods: {
     tobottom () {
-      let end = this.end + this.buffer
-      this.$socket.sendObj({ mutation: 'playlist', value: [this.end + 1, end] })
+      let end = this.end + this.bench
+      console.info('request', this.end, end)
+      this.$socket.sendObj({ mutation: 'playlist', value: [this.end, end] })
     },
 
     totop () {
-      let start = this.start - this.buffer
+      let start = this.start - this.bench
       if (start < 0) {
         start = 0
       }
-      this.$socket.sendObj({ mutation: 'playlist', value: [start, this.start - 1] })
+      console.info('request', start, this.start)
+      this.$socket.sendObj({ mutation: 'playlist', value: [start, this.start] })
     },
 
     onscroll (event, data) {
@@ -65,16 +68,20 @@ export default {
       this.end = data['end']
 
       if (
-        this.start > this.bufferedStart + this.buffer ||
-        this.end < this.bufferedEnd - this.buffer
+        this.start > this.bufferedEnd - this.buffer ||
+        this.end < this.bufferedStart + this.buffer
       ) {
-        console.info('request', this.start, this.end)
         this.bufferedStart = this.start
         this.bufferedEnd = this.end
 
+        console.info('request', this.start, this.end)
         this.$socket.sendObj({ mutation: 'playlist', value: [this.start, this.end] })
       }
     }
   }
 }
 </script>
+
+<style lang="stylus">
+
+</style>
