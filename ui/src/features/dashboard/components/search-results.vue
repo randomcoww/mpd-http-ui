d2ca92e45feaed297648cd039b31b381fa5d61b5
randomcoww <etherblimp@gmail.com>
2018-07-22 01:44:17 -0700

    Formatting for search results

--- ui/src/features/dashboard/components/search-results.vue
+++ ui/src/features/dashboard/components/search-results.vue
@@ -4,24 +4,33 @@ v-card
     .title Library
   v-card-text
     v-text-field(label="Search..." v-model.lazy="databasequery")
-    v-layout(row wrap style="align-items: center;")
-      template(v-for="searchresult in searchresults")
-        v-flex(d-flex xs12 sm12 md4)
-          | {{searchresult.artist}}
-        v-flex(d-flex xs12 sm12 md8)
-          v-layout(style="align-items: center;")
-            v-flex.text-xs-left(md10) {{searchresult.title}}
-            v-flex.text-xs-right(md2)
-              v-btn(flat icon color="primary")
-                v-icon delete
+    virtual-list(:size="64" :remain="this.buffer" :onscroll="onscroll" :tobottom="tobottom" :totop="totop")
+      ul(v-for="searchresult in searchresults")
+        v-flex(d-flex)
+          v-layout(row wrap style="align-items: center;")
+            v-flex(d-flex xs12 sm12 md4)
+              | {{searchresult.artist}}
+            v-flex(d-flex xs12 sm12 md8)
+              v-layout(style="align-items: center;")
+                v-flex.text-xs-left(md10) {{searchresult.title}}
+                v-flex.text-xs-left(md2)
+                  v-btn(flat icon color="primary")
+                    v-icon add
 </template>
 
 <script>
+import VirtualList from 'vue-virtual-scroll-list'
+
 export default {
+  components: {
+    VirtualList
+  },
+
   data () {
     return {
       databasequery: null,
-      errored: false
+      errored: false,
+      buffer: 15
     }
   },
 
@@ -34,6 +43,17 @@ export default {
   watch: {
     databasequery (after, before) {
       this.$socket.sendObj({ mutation: 'search', value: [this.databasequery, 100] })
+    }
+  },
+
+  methods: {
+    tobottom () {
+    },
+
+    totop () {
+    },
+
+    onscroll (event, data) {
     }
   }
 }
