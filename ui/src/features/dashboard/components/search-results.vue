3422da33d07127bb272aed0d6e37a3443edc3bdd
randomcoww <etherblimp@gmail.com>
2018-07-22 22:42:05 -0700

    Quick implementation of add to playlist from search

--- ui/src/features/dashboard/components/search-results.vue
+++ ui/src/features/dashboard/components/search-results.vue
@@ -5,28 +5,31 @@ v-card.searchresults
   v-card-text
     v-text-field(label="Search..." v-model.lazy="databasequery")
     virtual-list(:size="this.size" :remain="this.buffer" :onscroll="onscroll" :tobottom="tobottom")
-      ul(v-for="searchresult in searchresults")
-        v-flex(d-flex :style="style")
-          v-layout(row wrap style="align-items: center;")
+      div(v-for="(searchresult, index) in searchresults" :index="index" :key="searchresult.file")
+        draggable(v-model="searchresults" @end="onmoved" :options="{group: {name: 'playlistitems'}}" :id="searchresult.file")
+          v-flex(d-flex :style="style")
+            v-layout(row wrap style="align-items: center;")
 
-            v-flex(d-flex xs10 sm10 md10)
-              v-flex(d-flex xs12 sm12 md4)
-                | {{searchresult.artist}}
-              v-flex.text-xs-left(md8)
-                | {{searchresult.title}}
+              v-flex(d-flex xs10 sm10 md10)
+                v-flex(d-flex xs12 sm12 md4)
+                  | {{ searchresult.artist || 'No Artist' }}
+                v-flex.text-xs-left(md8)
+                  | {{ searchresult.title || 'No Title' }}
 
-            v-flex(d-flex xs2 sm2 md2)
-              v-layout(style="align-items: center;")
-                v-btn(flat icon color="primary")
-                  v-icon add
+              v-flex(d-flex xs2 sm2 md2)
+                v-layout(style="align-items: center;")
+                  v-btn(flat icon color="primary" @click="addpath(searchresult.file, -1)")
+                    v-icon add
 </template>
 
 <script>
 import VirtualList from 'vue-virtual-scroll-list'
+import draggable from 'vuedraggable'
 
 export default {
   components: {
-    VirtualList
+    VirtualList,
+    draggable
   },
 
   data () {
@@ -40,8 +43,12 @@ export default {
   },
 
   computed: {
-    searchresults () {
-      return this.$store.state.websocket.socket.search
+    searchresults: {
+      get: function () {
+        return this.$store.state.websocket.socket.search
+      },
+      set: function () {
+      }
     },
     style () {
       return {
@@ -57,6 +64,15 @@ export default {
   },
 
   methods: {
+    addpath (path, position) {
+      console.info('addpath', path, position)
+      this.$socket.sendObj({ mutation: 'addpath', value: [path, parseInt(position)] })
+    },
+
+    onmoved (event, data) {
+      this.addpath(event.from.id, event.to.id)
+    },
+
     tobottom () {
     },
 
