47b5104f5ddd039bb171b2d00907ebab1a9bf956
randomcoww <randomcoww@myhost-192-168-62-76.host.internal>
2018-08-06 23:57:21 -0700

    Fix running query for each broadcast client

--- api_server/server.go
+++ api_server/server.go
@@ -39,7 +39,7 @@ type Client struct {
 	conn *websocket.Conn
 
 	// Buffered channel of outbound messages.
-	send chan []byte
+	send chan *socketMessage
 }
 
 const (
@@ -57,7 +57,6 @@ type socketMessage struct {
 }
 
 func NewServer(listenUrl, mpdUrl, esUrl string) {
-
 	// backend stuff
 	mpdClient = mpd_handler.NewMpdClient("tcp", mpdUrl)
 	esClient = es_handler.NewEsClient(esUrl, esIndex, esDocument, "")
@@ -104,98 +103,60 @@ func NewServer(listenUrl, mpdUrl, esUrl string) {
 	log.Fatal(http.ListenAndServe(listenUrl, handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(r)))
 }
 
-func parseNum(input string) int {
-	v, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Printf("Error parsing param %s: %s\n", input, err)
-		v = -1
-	}
-
-	return v
-}
-
-func (h *Hub) eventBroadcaster() {
-	for {
-		select {
-		case e := <-mpdEvent.Event:
-			fmt.Printf("Got MPD event: %s\n", e)
-
-			switch e {
-			case "playlist":
-				// getPlaylistChanges()
-				// sendPlaylistChanges()
-				h.broadcast <- []byte(e)
-
-			default:
-				h.broadcast <- []byte(e)
-			}
-		}
-	}
-}
-
-func (c *Client) sendStatusMessage() {
+//
+// broadcast events
+//
+func createStatusMessage() (*socketMessage, error) {
 	attrs, err := mpdClient.Conn.Status()
 	if err != nil {
-		return
+		return nil, err
 	}
-
-	err = c.conn.WriteJSON(&socketMessage{Data: attrs, Name: "status"})
-	if err != nil {
-		fmt.Printf("Failed to write status message %s\n", err)
-		return
-	}
+	return &socketMessage{Data: attrs, Name: "status"}, nil
 }
 
-func (c *Client) sendCurrentSongMessage() {
+func createCurrentSongMessage() (*socketMessage, error) {
 	attrs, err := mpdClient.Conn.CurrentSong()
 	if err != nil {
-		return
+		return nil, err
 	}
-
-	err = c.conn.WriteJSON(&socketMessage{Data: attrs, Name: "currentsong"})
-	if err != nil {
-		fmt.Printf("Failed to write currentsong message %s\n", err)
-		return
-	}
+	return &socketMessage{Data: attrs, Name: "currentsong"}, nil
 }
 
-func updatePlaylistStatus() error {
+func createSeekMessage() (*socketMessage, error) {
 	attrs, err := mpdClient.Conn.Status()
 	if err != nil {
-		return err
+		return nil, err
 	}
+	switch attrs["state"] {
+	case "play":
+		message := make([]float64, 2)
 
-	playlistVersion, err = strconv.Atoi(attrs["playlist"])
-	if err != nil {
-		return err
+		elapsed, err := strconv.ParseFloat(attrs["elapsed"], 32)
+		if err != nil {
+			return nil, err
+		}
+
+		duration, err := strconv.ParseFloat(attrs["duration"], 32)
+		if err != nil {
+			return nil, err
+		}
+
+		message[0] = elapsed
+		message[1] = duration
+
+		return &socketMessage{Data: message, Name: "seek"}, nil
 	}
-
-	playlistLength, err = strconv.Atoi(attrs["playlistlength"])
-	if err != nil {
-		return err
-	}
-
-	return nil
-	// return playlist, playlistLength, nil
+	return nil, nil
 }
 
-// send playlist info
-func (c *Client) sendPlaylistUpdateMessage(start, end int) {
-	attrs, err := mpdClient.Conn.PlaylistInfo(start, end)
-	if err != nil {
-		return
-	}
-
-	err = c.conn.WriteJSON(&socketMessage{Data: attrs, Name: "playlistupdate"})
-	if err != nil {
-		fmt.Printf("Failed to write playlistupdate message %s\n", err)
-		return
-	}
+func createUpdateDatabaseMessage() *socketMessage {
+	return &socketMessage{Name: "updatedb"}
 }
 
-func (c *Client) sendPlaylistChanges() {
+func createPlaylistChangedMessage() (*socketMessage, error) {
 	curPlaylistLength := playlistLength
 	curPlaylistVersion := playlistVersion
+	// set new playlistVersion and playlistLength
 	updatePlaylistStatus()
 
 	fmt.Printf("MPD playlist update length: %v -> %v\n", curPlaylistLength, playlistLength)
@@ -215,7 +176,7 @@ func (c *Client) sendPlaylistChanges() {
 		changeStartPos, _, err := getPlaylistChangePos(curPlaylistVersion)
 
 		if err != nil {
-			return
+			return nil, err
 		}
 		// send socket event
 		// changeStartPos, changeStartPos + addCount
@@ -224,11 +185,7 @@ func (c *Client) sendPlaylistChanges() {
 		message[0] = changeStartPos
 		message[1] = addCount
 
-		err = c.conn.WriteJSON(&socketMessage{Data: message, Name: "playlistadd"})
-		if err != nil {
-			fmt.Printf("Failed to write playlist message %s\n", err)
-			return
-		}
+		return &socketMessage{Data: message, Name: "playlistadd"}, nil
 
 	} else if playlistLength < curPlaylistLength {
 		// Behavior for removed from playlist
@@ -242,7 +199,7 @@ func (c *Client) sendPlaylistChanges() {
 		changeStartPos, _, err := getPlaylistChangePos(curPlaylistVersion)
 
 		if err != nil {
-			return
+			return nil, err
 		}
 		// send socket event
 		// changeStartPos, changeStartPos + removeCount
@@ -251,11 +208,7 @@ func (c *Client) sendPlaylistChanges() {
 		message[0] = changeStartPos
 		message[1] = removeCount
 
-		err = c.conn.WriteJSON(&socketMessage{Data: message, Name: "playlistdelete"})
-		if err != nil {
-			fmt.Printf("Failed to write playlist message %s\n", err)
-			return
-		}
+		return &socketMessage{Data: message, Name: "playlistdelete"}, nil
 
 	} else {
 		// Fallback for generic playlist changes (move, shuffle, etc)
@@ -263,7 +216,7 @@ func (c *Client) sendPlaylistChanges() {
 		changeCount := changeEndPos - changeStartPos + 1
 
 		if err != nil {
-			return
+			return nil, err
 		}
 
 		fmt.Printf("MPD playlist moved positions at: %v count: %v\n", changeStartPos, changeCount)
@@ -271,12 +224,337 @@ func (c *Client) sendPlaylistChanges() {
 		message[0] = changeStartPos
 		message[1] = changeCount
 
-		err = c.conn.WriteJSON(&socketMessage{Data: message, Name: "playlistmove"})
-		if err != nil {
-			fmt.Printf("Failed to write playlist message %s\n", err)
-			return
+		return &socketMessage{Data: message, Name: "playlistmove"}, nil
+	}
+}
+
+//
+// client specific events
+//
+func createPlaylistQueryMessage(start, end int) (*socketMessage, error) {
+	attrs, err := mpdClient.Conn.PlaylistInfo(start, end)
+	if err != nil {
+		return nil, err
+	}
+	return &socketMessage{Data: attrs, Name: "playlistupdate"}, nil
+}
+
+func createSearchMessage(query string, start, size int) (*socketMessage, error) {
+	search, err := esClient.Search(query, start, size)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*json.RawMessage
+	for _, hits := range search.Hits.Hits {
+		result = append(result, hits.Source)
+	}
+
+	message := make([]interface{}, 2)
+	message[0] = result
+	message[1] = start
+	return &socketMessage{Data: message, Name: "search"}, nil
+}
+
+//
+// broadcaster
+//
+func (h *Hub) eventBroadcaster() {
+	for {
+		select {
+		case e := <-mpdEvent.Event:
+			fmt.Printf("Got MPD event: %s\n", e)
+
+			switch e {
+			case "player":
+				msg, err := createStatusMessage()
+				if err != nil {
+					break
+				}
+				h.broadcast <- msg
+
+				msg, err = createCurrentSongMessage()
+				if err != nil {
+					break
+				}
+				h.broadcast <- msg
+
+			case "playlist":
+				msg, err := createPlaylistChangedMessage()
+				if err != nil {
+					break
+				}
+				h.broadcast <- msg
+
+			case "mixer", "options", "outputs":
+				msg, err := createStatusMessage()
+				if err != nil {
+					break
+				}
+				h.broadcast <- msg
+
+			case "update":
+				msg := createUpdateDatabaseMessage()
+				h.broadcast <- msg
+			}
+
+		case <-time.After(1000 * time.Millisecond):
+			msg, err := createSeekMessage()
+			if err != nil {
+				break
+			}
+			h.broadcast <- msg
 		}
 	}
+}
+
+//
+// send broadcast events to each client
+//
+func (c *Client) writeSocketEvents() {
+
+	defer func() {
+		fmt.Printf("Close writer\n")
+		c.conn.Close()
+	}()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("Recovered %s\n", r)
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.send:
+			if !ok {
+				// The hub closed the channel.
+				fmt.Printf("Hub closed the channel\n")
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			c.conn.WriteJSON(*msg)
+		}
+	}
+}
+
+//
+// read messages from client
+//
+func (c *Client) readSocketEvents() {
+
+	defer func() {
+		fmt.Printf("Close reader\n")
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			time.Sleep(1000 * time.Millisecond)
+			fmt.Printf("Recovered %s\n", r)
+		}
+	}()
+
+	for {
+		v := &socketMessage{}
+
+		err := c.conn.ReadJSON(v)
+		if err != nil {
+			fmt.Printf("Error reading socket %s\n", err)
+
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+
+		switch v.Name {
+		case "seek":
+			t := int64(v.Data.(float64) * 1000000000)
+			mpdClient.Conn.SeekCur(time.Duration(t), false)
+
+			// client specific playlist query
+		case "playlistupdate":
+			d := v.Data.([]interface{})
+			start := int(d[0].(float64))
+			end := int(d[1].(float64))
+			msg, err := createPlaylistQueryMessage(start, end)
+			if err != nil {
+				break
+			}
+			c.conn.WriteJSON(*msg)
+
+			// client specific current song query
+		case "currentsong":
+			msg, err := createCurrentSongMessage()
+			if err != nil {
+				break
+			}
+			c.conn.WriteJSON(*msg)
+
+			// global playlist items moved
+			// send only and allow server to emit event
+		case "playlistmove":
+			d := v.Data.([]interface{})
+			// fmt.Printf("Move %s\n", d)
+			start := int(d[0].(float64))
+			end := int(d[1].(float64))
+			position := int(d[2].(float64))
+			// fmt.Printf("Move %s %s %s\n")
+			if start != position {
+				mpdClient.Conn.Move(start, end, position)
+			}
+
+		case "playid":
+			// -1 for play current
+			d := int(v.Data.(float64))
+			mpdClient.Conn.PlayID(d)
+
+		case "stop":
+			mpdClient.Conn.Stop()
+
+		case "pause":
+			mpdClient.Conn.Pause(true)
+
+		case "playnext":
+			mpdClient.Conn.Next()
+
+		case "playprev":
+			mpdClient.Conn.Previous()
+
+		case "removeid":
+			d := int(v.Data.(float64))
+			mpdClient.Conn.DeleteID(d)
+
+		case "addpath":
+			d := v.Data.([]interface{})
+			// fmt.Printf("Move %s\n", d)
+			path := d[0].(string)
+			position := int(d[1].(float64))
+			mpdClient.Conn.AddID(path, position)
+
+			// client specific database search
+		case "search":
+			d := v.Data.([]interface{})
+			query := d[0].(string)
+			start := int(d[1].(float64))
+			size := int(d[2].(float64))
+			// fmt.Printf("Search %s %s %s\n", query, start, size)
+			msg, err := createSearchMessage(query, start, size)
+			if err != nil {
+				break
+			}
+			c.conn.WriteJSON(*msg)
+
+		case "clear":
+			mpdClient.Conn.Clear()
+
+		case "updatedb":
+			mpdClient.Conn.Update("")
+		}
+	}
+}
+
+//
+// web socket feeder
+// based on example https://github.com/gorilla/websocket/blob/master/examples/filewatch/main.go
+//
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("Serving WS\n")
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		if _, ok := err.(websocket.HandshakeError); !ok {
+			log.Printf("%s\n", err)
+		}
+
+		fmt.Printf("Serving WS err %s\n", err)
+		return
+	}
+
+	client := &Client{
+		hub:  hub,
+		conn: ws,
+		send: make(chan *socketMessage, 256),
+	}
+
+	client.hub.register <- client
+	go client.writeSocketEvents()
+	go client.readSocketEvents()
+}
+
+//
+// http handle funcs
+//
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Healthcheck\n")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response{"ok"})
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Printf("Search database %s\n", params)
+
+	search, err := esClient.Search(
+		params["query"],
+		parseNum(params["start"]),
+		parseNum(params["size"]))
+	w.Header().Set("Content-Type", "application/json")
+
+	if err == nil {
+		w.WriteHeader(http.StatusOK)
+
+		// var	result []*json.RawMessage
+		// for _, hits := range search.Hits.Hits {
+		// 	result = append(result, hits.Source)
+		// }
+
+		json.NewEncoder(w).Encode(search)
+	} else {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(response{err.Error()})
+	}
+}
+
+//
+// helpers
+//
+func parseNum(input string) int {
+	v, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Printf("Error parsing param %s: %s\n", input, err)
+		v = -1
+	}
+
+	return v
+}
+
+// update global playlistVersion and playlistLength
+func updatePlaylistStatus() error {
+	attrs, err := mpdClient.Conn.Status()
+	if err != nil {
+		return err
+	}
+
+	playlistVersion, err = strconv.Atoi(attrs["playlist"])
+	if err != nil {
+		return err
+	}
+
+	playlistLength, err = strconv.Atoi(attrs["playlistlength"])
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // when playlist changes, get the start and end index of change
@@ -311,282 +589,4 @@ func getPlaylistChangePos(playlistVersion int) (int, int, error) {
 	}
 
 	return startPos, endPos, nil
-}
-
-func (c *Client) sendSeekMessage() {
-	attrs, err := mpdClient.Conn.Status()
-	if err != nil {
-		return
-	}
-
-	switch attrs["state"] {
-	case "play":
-		message := make([]float64, 2)
-
-		elapsed, err := strconv.ParseFloat(attrs["elapsed"], 32)
-		if err != nil {
-			return
-		}
-
-		duration, err := strconv.ParseFloat(attrs["duration"], 32)
-		if err != nil {
-			return
-		}
-
-		message[0] = elapsed
-		message[1] = duration
-
-		c.conn.WriteJSON(&socketMessage{Data: message, Name: "seek"})
-	}
-}
-
-func (c *Client) sendSearchMessage(query string, start, size int) {
-	search, err := esClient.Search(query, start, size)
-	if err != nil {
-		return
-	}
-
-	message := make([]interface{}, 2)
-
-	var result []*json.RawMessage
-	for _, hits := range search.Hits.Hits {
-		result = append(result, hits.Source)
-	}
-
-	message[0] = result
-	message[1] = start
-
-	err = c.conn.WriteJSON(&socketMessage{Data: message, Name: "search"})
-	if err != nil {
-		fmt.Printf("Failed to write search message %s\n", err)
-		return
-	}
-}
-
-func (c *Client) sendUpdateDatabaseMessage() {
-	c.conn.WriteJSON(&socketMessage{Name: "updatedb"})
-}
-
-func (c *Client) readSocketEvents() {
-
-	defer func() {
-		fmt.Printf("Close reader\n")
-		c.hub.unregister <- c
-		c.conn.Close()
-	}()
-
-	defer func() {
-		r := recover()
-		if r != nil {
-			time.Sleep(1000 * time.Millisecond)
-			fmt.Printf("Recovered %s\n", r)
-		}
-	}()
-
-	for {
-		v := &socketMessage{}
-
-		err := c.conn.ReadJSON(v)
-		if err != nil {
-			fmt.Printf("Error reading socket %s\n", err)
-
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
-
-		switch v.Name {
-		case "seek":
-			t := int64(v.Data.(float64) * 1000000000)
-			mpdClient.Conn.SeekCur(time.Duration(t), false)
-			c.sendSeekMessage()
-
-		case "playlistupdate":
-			d := v.Data.([]interface{})
-			start := int(d[0].(float64))
-			end := int(d[1].(float64))
-			// respond with playlist
-			c.sendPlaylistUpdateMessage(start, end)
-
-		case "currentsong":
-			// respond with current song
-			c.sendCurrentSongMessage()
-
-		case "playlistmove":
-			d := v.Data.([]interface{})
-			// fmt.Printf("Move %s\n", d)
-			start := int(d[0].(float64))
-			end := int(d[1].(float64))
-			position := int(d[2].(float64))
-			// fmt.Printf("Move %s %s %s\n")
-			if start != position {
-				mpdClient.Conn.Move(start, end, position)
-			}
-
-		// case "play":
-		//   // play current
-		// 	mpdClient.Conn.PlayID(-1)
-
-		case "playid":
-			// -1 for play current
-			d := int(v.Data.(float64))
-			mpdClient.Conn.PlayID(d)
-
-		case "stop":
-			mpdClient.Conn.Stop()
-
-		case "pause":
-			mpdClient.Conn.Pause(true)
-
-		case "playnext":
-			mpdClient.Conn.Next()
-
-		case "playprev":
-			mpdClient.Conn.Previous()
-
-		case "removeid":
-			d := int(v.Data.(float64))
-			mpdClient.Conn.DeleteID(d)
-
-		case "addpath":
-			d := v.Data.([]interface{})
-			// fmt.Printf("Move %s\n", d)
-			path := d[0].(string)
-			position := int(d[1].(float64))
-			mpdClient.Conn.AddID(path, position)
-
-		case "search":
-			d := v.Data.([]interface{})
-			query := d[0].(string)
-			start := int(d[1].(float64))
-			size := int(d[2].(float64))
-			// fmt.Printf("Search %s %s %s\n", query, start, size)
-			c.sendSearchMessage(query, start, size)
-
-		case "clear":
-			mpdClient.Conn.Clear()
-
-		case "updatedb":
-			mpdClient.Conn.Update("")
-		}
-	}
-}
-
-func (c *Client) writeSocketEvents() {
-
-	defer func() {
-		fmt.Printf("Close writer\n")
-		c.conn.Close()
-	}()
-
-	defer func() {
-		r := recover()
-		if r != nil {
-			fmt.Printf("Recovered %s\n", r)
-			time.Sleep(1000 * time.Millisecond)
-		}
-	}()
-
-	for {
-		select {
-		case message, ok := <-c.send:
-			if !ok {
-				// The hub closed the channel.
-				fmt.Printf("Hub closed the channel\n")
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			switch string(message) {
-			case "player":
-				c.sendStatusMessage()
-				c.sendCurrentSongMessage()
-
-			case "mixer":
-				c.sendStatusMessage()
-
-			case "options":
-				c.sendStatusMessage()
-
-			case "outputs":
-				c.sendStatusMessage()
-
-			case "playlist":
-				// c.sendPlaylistMessage()
-				c.sendPlaylistChanges()
-
-			case "update":
-				c.sendUpdateDatabaseMessage()
-			}
-
-		case <-time.After(1000 * time.Millisecond):
-			c.sendSeekMessage()
-		}
-	}
-}
-
-//
-// web socket feeder
-// based on example https://github.com/gorilla/websocket/blob/master/examples/filewatch/main.go
-//
-func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
-	// fmt.Printf("Serving WS\n")
-
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			log.Printf("%s\n", err)
-		}
-
-		fmt.Printf("Serving WS err %s\n", err)
-		return
-	}
-
-	client := &Client{
-		hub:  hub,
-		conn: ws,
-		send: make(chan []byte, 256),
-	}
-
-	client.hub.register <- client
-	go client.writeSocketEvents()
-	go client.readSocketEvents()
-}
-
-//
-// handle funcs
-//
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Healthcheck\n")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response{"ok"})
-}
-
-func search(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	fmt.Printf("Search database %s\n", params)
-
-	search, err := esClient.Search(
-		params["query"],
-		parseNum(params["start"]),
-		parseNum(params["size"]))
-	w.Header().Set("Content-Type", "application/json")
-
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-
-		// var	result []*json.RawMessage
-		// for _, hits := range search.Hits.Hits {
-		// 	result = append(result, hits.Source)
-		// }
-
-		json.NewEncoder(w).Encode(search)
-	} else {
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		json.NewEncoder(w).Encode(response{err.Error()})
-	}
 }
